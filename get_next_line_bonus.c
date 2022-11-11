7b384b7433aac9bdea114200f64dc7bc102fcb75
Oussama Krich <okrich@e2r3p2.1337.ma>
2022-11-11 19:03:17 +0100

    fnl

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,12 +6,12 @@
 /*   By: okrich <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 15:23:45 by okrich            #+#    #+#             */
-/*   Updated: 2022/11/11 17:19:54 by okrich           ###   ########.fr       */
+/*   Updated: 2022/11/11 19:01:11 by okrich           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
-#include <sys/fcntl.h>
+#include <stdio.h>
 #include <sys/syslimits.h>
 
 static int	check_newline(char *reader)
@@ -82,25 +82,27 @@ static int	read_and_get_line(int fd, char **line, char **rest)
 	return (free(reader[fd]), n);
 }
 
-char	*get_next_line_bonus(int fd)
+char	*get_next_line(int fd)
 {
-	static char	*rest;
-	char		*line;
+	static char	*rest[OPEN_MAX];
+	char		*line[OPEN_MAX];
 	ssize_t		n;
 
-	line = NULL;
-	n = check_newline(rest);
-	if (rest != NULL && n != -1)
+	if (fd < 0 || fd > OPEN_MAX)
+		return (NULL);
+	line[fd] = NULL;
+	n = check_newline(rest[fd]);
+	if (rest[fd] != NULL && n != -1)
 	{		
-		line = get_resteofline(n, &rest, line);
-		return (line);
+		line[fd] = get_resteofline(n, &rest[fd], line[fd]);
+		return (line[fd]);
 	}
-	line = rest;
-	rest = NULL;
-	n = read_and_get_line(fd, &line, &rest);
+	line[fd] = rest[fd];
+	rest[fd] = NULL;
+	n = read_and_get_line(fd, &line[fd], &rest[fd]);
 	if (n == -1)
-		return (free(line), NULL);
-	return (line);
+		return (free(line[fd]), NULL);
+	return (line[fd]);
 }
 //
 // void my_exit()
@@ -112,14 +114,19 @@ char	*get_next_line_bonus(int fd)
 // {
 // 	int fd =  open("txt1",O_RDONLY);
 // 	int fd2 = open("txt2",O_RDONLY);
-// 	char *str;
-// 	char *str2;
-// 	while((str = get_next_line_bonus(fd)) && (str2 = get_next_line_bonus(fd2)))
+// 	// char *str;
+// 	// char *str2;
+// 	int i = 0;
+// 	while( i < 4 )//(str = get_next_line(fd)) && (str2 = get_next_line(fd2)))
 // 	{
-// 		printf("%s",str2);
-// 		printf("%s",str);
-// 		free(str);
-// 		free(str2);
+// 		printf("%s",get_next_line(fd));
+// 		printf("%s",get_next_line(fd2));
+// 		// free(str);
+// 		// free(str2);
+// 		i++;
 // 	}
-// 	atexit(my_exit);
+// 	// printf("%s",get_next_line(10240));
+// 	// printf("%s",get_next_line(13));
+//
+// 	// atexit(my_exit);
 // }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,23 +6,21 @@
 /*   By: okrich <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:54:45 by okrich            #+#    #+#             */
-/*   Updated: 2022/11/11 16:57:09 by okrich           ###   ########.fr       */
+/*   Updated: 2022/11/11 17:51:15 by okrich           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-# endif
 # include <sys/fcntl.h>
 # include <stddef.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <sys/fcntl.h>
+# include <sys/syslimits.h>
 
 void	ft_free(char **ptr);
 char	*ft_strnjoin(char *s1, char *s2, int i);
@@ -30,7 +28,6 @@ char	*ft_strdup(char *s1);
 char	*ft_strndup(char *s1, int n);
 int		ft_strlen(char *str);
 char	*ft_substr(char *s, int start, int len);
-char	*get_next_line_bonus(int fd);
+char	*get_next_line(int fd);
 
 #endif
-
